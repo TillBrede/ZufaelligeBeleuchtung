0cb74f0cf8a3b1387d16a16fad7db2b719426b84
Till Brede <tb@symcon.de>
2019-10-11 12:40:35 +0200

    applied cs fixer style

--- ZufaelligeBeleuchtung/module.php
+++ ZufaelligeBeleuchtung/module.php
@@ -1,141 +1,142 @@
 <?php
-	class ZufaelligeBeleuchtung extends IPSModule {
 
-		public function Create() {
-			//Never delete this line!
-			parent::Create();
+declare(strict_types=1);
+class ZufaelligeBeleuchtung extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
 
-			//Properties
-			$this->RegisterPropertyString('Targets', '[]');
-			$this->RegisterPropertyString('SwitchColors', '[{"Color":242944}, {"Color":16711680}, {"Color":16777215}]');
-			$this->RegisterPropertyInteger('Interval', 1);
-			$this->RegisterPropertyBoolean('SimultaneousSwitching', false);
-			
-			//Variables
-			$this->RegisterVariableBoolean('Active', $this->Translate('Active'), '~Switch');
-			$this->EnableAction('Active');
+        //Properties
+        $this->RegisterPropertyString('Targets', '[]');
+        $this->RegisterPropertyString('SwitchColors', '[{"Color":242944}, {"Color":16711680}, {"Color":16777215}]');
+        $this->RegisterPropertyInteger('Interval', 1);
+        $this->RegisterPropertyBoolean('SimultaneousSwitching', false);
 
-			//Timer
-			$this->RegisterTimer('ChangeTimer', 0, 'ZB_ChangeLight($_IPS[\'TARGET\']);');
+        //Variables
+        $this->RegisterVariableBoolean('Active', $this->Translate('Active'), '~Switch');
+        $this->EnableAction('Active');
 
-			//Attribute
-			$this->RegisterAttributeString('BaseValues', '[]');
-		}
+        //Timer
+        $this->RegisterTimer('ChangeTimer', 0, 'ZB_ChangeLight($_IPS[\'TARGET\']);');
 
-		public function Destroy(){
-			//Never delete this line!
-			parent::Destroy();
+        //Attribute
+        $this->RegisterAttributeString('BaseValues', '[]');
+    }
 
-		}
+    public function Destroy()
+    {
+        //Never delete this line!
+        parent::Destroy();
+    }
 
-		public function ApplyChanges() {
-			//Never delete this line!
-			parent::ApplyChanges();
-			
-			//Deleting references in order to re-add them
-			foreach ($this->GetReferenceList() as $referenceID) {
-				$this->UnregisterReference($referenceID);
-			}
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
 
-			//Adding references
-			$targetList = json_decode($this->ReadPropertyString('Targets'), true);
-			foreach ($targetList as $line) {
-				$this->RegisterReference($line['VariableID']);
-			}
+        //Deleting references in order to re-add them
+        foreach ($this->GetReferenceList() as $referenceID) {
+            $this->UnregisterReference($referenceID);
+        }
 
-			if(GetValue($this->GetIDForIdent('Active'))) {
-				SetValue($this->GetIDForIdent('Active'), GetValue($this->GetIDForIdent('Active')));
-				$this->ChangeLight();
-				$this->SetTimerInterval('ChangeTimer', $this->ReadPropertyInteger('Interval') * 1000);
-			} else {
-				SetValue($this->GetIDForIdent('Active'), GetValue($this->GetIDForIdent('Active')));
-				$this->SetTimerInterval('ChangeTimer', 0);
-			}
-		}
+        //Adding references
+        $targetList = json_decode($this->ReadPropertyString('Targets'), true);
+        foreach ($targetList as $line) {
+            $this->RegisterReference($line['VariableID']);
+        }
 
-		public function RequestAction($Ident, $Value)
-		{
-			switch ($Ident) {
-				case 'Active':
-					$this->SetActive($Value);
-					break;
-				default:
-					throw new Exception('Invalid ident');
-			}
-		}
+        if (GetValue($this->GetIDForIdent('Active'))) {
+            SetValue($this->GetIDForIdent('Active'), GetValue($this->GetIDForIdent('Active')));
+            $this->ChangeLight();
+            $this->SetTimerInterval('ChangeTimer', $this->ReadPropertyInteger('Interval') * 1000);
+        } else {
+            SetValue($this->GetIDForIdent('Active'), GetValue($this->GetIDForIdent('Active')));
+            $this->SetTimerInterval('ChangeTimer', 0);
+        }
+    }
 
-		private function SetActive($Active) 
-		{
-			if($Active) {
-				SetValue($this->GetIDForIdent('Active'), $Active);
-				$this->SaveBaseValues();
-				$this->ChangeLight();
-				$this->SetTimerInterval('ChangeTimer', $this->ReadPropertyInteger('Interval') * 1000);
-			} else {
-				SetValue($this->GetIDForIdent('Active'), $Active);
-				$this->ResetValues();
-				$this->SetTimerInterval('ChangeTimer', 0);
-			}
-			
-		}
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+                case 'Active':
+                    $this->SetActive($Value);
+                    break;
+                default:
+                    throw new Exception('Invalid ident');
+            }
+    }
 
-		public function ChangeLight()
-		{
-			//Creating array with targetIDs
-			$targetList = json_decode($this->ReadPropertyString('Targets'), true);
-			$targetIDs = [];
-			foreach ($targetList as $line) {
-				$targetIDs[] = $line['VariableID'];
-			}
+    private function SetActive($Active)
+    {
+        if ($Active) {
+            SetValue($this->GetIDForIdent('Active'), $Active);
+            $this->SaveBaseValues();
+            $this->ChangeLight();
+            $this->SetTimerInterval('ChangeTimer', $this->ReadPropertyInteger('Interval') * 1000);
+        } else {
+            SetValue($this->GetIDForIdent('Active'), $Active);
+            $this->ResetValues();
+            $this->SetTimerInterval('ChangeTimer', 0);
+        }
+    }
 
-			//Creating array witch colorValues
-			$colorList = json_decode($this->ReadPropertyString('SwitchColors'), true);
-			$colorValues = [];
-			foreach ($colorList as $line) {
-				$colorValues[] = $line['Color'];
-			}
-			
-			if ($this->ReadPropertyBoolean('SimultaneousSwitching')) {
-				$colorIndex = random_int(0, count($colorValues) - 1);
-				while ($colorValues[$colorIndex] == GetValue($targetIDs[0])) {
-					$colorIndex = random_int(0, count($colorValues) - 1);
-				}
-				foreach($targetIDs as $targetID) {
-					RequestAction($targetID, $colorValues[$colorIndex]);
-				}	
-			} else {
-				foreach($targetIDs as $targetID) {
-					$colorIndex = random_int(0, count($colorValues) - 1);
-					while ($colorValues[$colorIndex] == GetValue($targetID)) {
-						$colorIndex = random_int(0, count($colorValues) - 1);
-					}
-					RequestAction($targetID, $colorValues[$colorIndex]);
-				}
-			}		
-		}
+    public function ChangeLight()
+    {
+        //Creating array with targetIDs
+        $targetList = json_decode($this->ReadPropertyString('Targets'), true);
+        $targetIDs = [];
+        foreach ($targetList as $line) {
+            $targetIDs[] = $line['VariableID'];
+        }
 
-		private function SaveBaseValues()
-		{
-			//Creating array with targetIDs
-			$this->WriteAttributeString('BaseValues', '[]');
-			$targetList = json_decode($this->ReadPropertyString('Targets'), true);
-			$baseValues = [];
-			foreach ($targetList as $line) {
-				$baseValues[$line['VariableID']] = GetValue($line['VariableID']);
-			}
-			$this->WriteAttributeString('BaseValues', json_encode($baseValues));
-		}
+        //Creating array witch colorValues
+        $colorList = json_decode($this->ReadPropertyString('SwitchColors'), true);
+        $colorValues = [];
+        foreach ($colorList as $line) {
+            $colorValues[] = $line['Color'];
+        }
 
-		private function ResetValues()
-		{
-			//Creating array with targetIDs
-			$targetList = json_decode($this->ReadPropertyString('Targets'), true);
-			$baseValues = json_decode($this->ReadAttributeString('BaseValues'), true);
-			foreach ($targetList as $line) {
-				RequestAction($line['VariableID'], $baseValues[$line['VariableID']]);
-			}
-		}
+        if ($this->ReadPropertyBoolean('SimultaneousSwitching')) {
+            $colorIndex = random_int(0, count($colorValues) - 1);
+            while ($colorValues[$colorIndex] == GetValue($targetIDs[0])) {
+                $colorIndex = random_int(0, count($colorValues) - 1);
+            }
+            foreach ($targetIDs as $targetID) {
+                RequestAction($targetID, $colorValues[$colorIndex]);
+            }
+        } else {
+            foreach ($targetIDs as $targetID) {
+                $colorIndex = random_int(0, count($colorValues) - 1);
+                while ($colorValues[$colorIndex] == GetValue($targetID)) {
+                    $colorIndex = random_int(0, count($colorValues) - 1);
+                }
+                RequestAction($targetID, $colorValues[$colorIndex]);
+            }
+        }
+    }
 
-	}
+    private function SaveBaseValues()
+    {
+        //Creating array with targetIDs
+        $this->WriteAttributeString('BaseValues', '[]');
+        $targetList = json_decode($this->ReadPropertyString('Targets'), true);
+        $baseValues = [];
+        foreach ($targetList as $line) {
+            $baseValues[$line['VariableID']] = GetValue($line['VariableID']);
+        }
+        $this->WriteAttributeString('BaseValues', json_encode($baseValues));
+    }
 
+    private function ResetValues()
+    {
+        //Creating array with targetIDs
+        $targetList = json_decode($this->ReadPropertyString('Targets'), true);
+        $baseValues = json_decode($this->ReadAttributeString('BaseValues'), true);
+        foreach ($targetList as $line) {
+            RequestAction($line['VariableID'], $baseValues[$line['VariableID']]);
+        }
+    }
+}
 
