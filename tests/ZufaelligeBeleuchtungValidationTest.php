e86bb6889c678a60d10661f7f4ee769db9a1b527
Till Brede <tb@symcon.de>
2019-10-11 12:50:37 +0200

    added validation test

---
+++ tests/ZufaelligeBeleuchtungValidationTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class ZufaelligeBeleuchtungValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateZufaelligeBeleuchtung(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateZufaelligeBeleuchtungModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../ZufaelligeBeleuchtung');
+    }
+}
